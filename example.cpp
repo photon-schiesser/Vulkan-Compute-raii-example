66d17b20373ef7b7242928fb81aab8f2e517cf78
Eric Schiesser <eric.schiesser@rochester.edu>
2022-08-14 14:54:07 -0400

    Enumerate physical devices

--- example.cpp
+++ example.cpp
@@ -30,12 +30,34 @@
 #define BAIL_ON_BAD_RESULT(result) \
   if ((result) != VK_SUCCESS) { fprintf(stderr, "Failure at %u %s\n", __LINE__, __FILE__); exit(-1); }
   
-int main()
-{
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr,&extensionCount, nullptr);
-    std::cout << extensionCount << " extensions supported\n";
-    BAIL_ON_BAD_RESULT(1);
+int main(int argc, const char * const argv[]) {
+  (void)argc;
+  (void)argv;
 
-    return 0;
+  const VkApplicationInfo applicationInfo = {
+    VK_STRUCTURE_TYPE_APPLICATION_INFO,
+    0,
+    "VKComputeSample",
+    0,
+    "",
+    0,
+    VK_MAKE_VERSION(1, 0, 9)
+  };
+  
+  const VkInstanceCreateInfo instanceCreateInfo = {
+    VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+    0,
+    0,
+    &applicationInfo,
+    0,
+    0,
+    0,
+    0
+  };
+  
+  VkInstance instance;
+  BAIL_ON_BAD_RESULT(vkCreateInstance(&instanceCreateInfo, 0, &instance));
+  
+  uint32_t physicalDeviceCount = 0;
+  BAIL_ON_BAD_RESULT(vkEnumeratePhysicalDevices(instance, &physicalDeviceCount, 0));
 }
