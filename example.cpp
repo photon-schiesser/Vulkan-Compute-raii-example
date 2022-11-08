e0380694ce18417d0a59b0edfbabd5f51900693c
Eric Schiesser <ericschiesser@gmail.com>
2022-11-08 07:32:18 -0500

    Use SPIR-V from pre-compiled file

--- example.cpp
+++ example.cpp
@@ -1,5 +1,3 @@
-#include "makeSpirvCode.hpp"
-
 #include <array>
 #include <fstream>
 #include <iostream>
@@ -12,11 +10,11 @@
 #include "vulkan/vulkan.hpp"
 #include "vulkan/vulkan_raii.hpp"
 
-#define BAIL_ON_BAD_RESULT(result)                                                                                     \
-    if ((result) != VK_SUCCESS)                                                                                        \
-    {                                                                                                                  \
-        fprintf(stderr, "Failure at %u %s\n", __LINE__, __FILE__);                                                     \
-        exit(-1);                                                                                                      \
+#define BAIL_ON_BAD_RESULT(result)                                                                 \
+    if ((result) != VK_SUCCESS)                                                                    \
+    {                                                                                              \
+        fprintf(stderr, "Failure at %u %s\n", __LINE__, __FILE__);                                 \
+        exit(-1);                                                                                  \
     }
 
 namespace
@@ -26,7 +24,8 @@ std::pair<VkResult, std::optional<uint32_t>> getBestComputeQueue(const auto& phy
     const auto queueFamilyProperties = physicalDevice.getQueueFamilyProperties();
     using queueIndex_t = uint32_t;
     // first try and find a queue that has just the compute bit set
-    const auto queueIndices = std::views::iota(queueIndex_t(0), queueIndex_t(queueFamilyProperties.size()));
+    const auto queueIndices =
+        std::views::iota(queueIndex_t(0), queueIndex_t(queueFamilyProperties.size()));
     for (const auto queueIndex : queueIndices)
     {
         const auto& prop = queueFamilyProperties[queueIndex];
@@ -35,7 +34,8 @@ std::pair<VkResult, std::optional<uint32_t>> getBestComputeQueue(const auto& phy
         const auto maskedFlags =
             (~(vk::QueueFlagBits::eTransfer | vk::QueueFlagBits::eSparseBinding) & prop.queueFlags);
 
-        if (!(vk::QueueFlagBits::eGraphics & maskedFlags) && (vk::QueueFlagBits::eCompute & maskedFlags))
+        if (!(vk::QueueFlagBits::eGraphics & maskedFlags) &&
+            (vk::QueueFlagBits::eCompute & maskedFlags))
         {
             return {VK_SUCCESS, queueIndex};
         }
@@ -60,6 +60,38 @@ std::pair<VkResult, std::optional<uint32_t>> getBestComputeQueue(const auto& phy
 }
 } // namespace
 
+inline auto div_up(uint32_t x, uint32_t y)
+{
+    return (x + y - 1u) / y;
+}
+
+auto getSpirvFromFile(const std::string_view filePath)
+{
+    std::vector<uint32_t> spirvFromFile;
+    std::vector<char> spvBuffer;
+    {
+        std::ifstream spirvFile(filePath.data(), std::ios::binary | std::ios::ate);
+        std::streamsize size = spirvFile.tellg();
+        spirvFile.seekg(0, std::ios::beg);
+
+        spvBuffer.resize(size);
+
+        if (!spirvFile.read(spvBuffer.data(), size))
+        {
+            std::cout << "Could not read spv file"
+                      << "\n";
+            spirvFile.close();
+            exit(1);
+        }
+        spirvFile.close();
+    }
+    spvBuffer.resize(4 * div_up(spvBuffer.size(), 4));
+    spirvFromFile.resize(spvBuffer.size() * sizeof(char) / sizeof(uint32_t));
+    const auto start = reinterpret_cast<uint32_t*>(spvBuffer.data());
+    std::copy(start, start + spirvFromFile.size(), spirvFromFile.data());
+    return spirvFromFile;
+}
+
 int main()
 {
     constexpr vk::ApplicationInfo applicationInfo = []() {
@@ -73,23 +105,17 @@ int main()
     }();
 
     constexpr std::array layers = {"VK_LAYER_KHRONOS_validation"};
-    const vk::InstanceCreateInfo instanceCreateInfo(vk::InstanceCreateFlags(), &applicationInfo, layers.size(),
-                                                    layers.data());
+    const vk::InstanceCreateInfo instanceCreateInfo(vk::InstanceCreateFlags(), &applicationInfo,
+                                                    layers.size(), layers.data());
 
     const vk::raii::Context context;
     const auto instance = vk::raii::Instance(context, instanceCreateInfo);
 
     constexpr uint32_t bufferLength = 16384 * 2;
     constexpr uint32_t bufferSize = sizeof(int32_t) * bufferLength;
-    constexpr auto spirv = makeSpirvCode(bufferSize);
     constexpr auto memorySize = bufferSize * 2;
 
-    // write the spirv to a file just to look at it. Feel free to comment this block out.
-    {
-        std::ofstream spirvFile("shader.comp.spirv", std::ios::binary);
-        spirvFile.write(reinterpret_cast<const char*>(spirv.data()), sizeof(spirv));
-        spirvFile.close();
-    }
+    const auto spirvFromFile = getSpirvFromFile("copy.comp.spv");
 
     const auto physicalDevices = instance.enumeratePhysicalDevices();
     for (const auto& physDev : physicalDevices)
@@ -102,8 +128,8 @@ int main()
         }
 
         constexpr std::array queuePrioritory = {1.0f};
-        const auto deviceQueueCreateInfo =
-            vk::DeviceQueueCreateInfo(vk::DeviceQueueCreateFlags(), *queueFamilyIndex, queuePrioritory);
+        const auto deviceQueueCreateInfo = vk::DeviceQueueCreateInfo(
+            vk::DeviceQueueCreateFlags(), *queueFamilyIndex, queuePrioritory);
 
         const std::array queueInfos = {deviceQueueCreateInfo};
         const auto deviceCreateInfo = vk::DeviceCreateInfo(vk::DeviceCreateFlags(), queueInfos);
@@ -115,8 +141,10 @@ int main()
             for (const auto& k : std::views::iota(0u, props.memoryTypeCount))
             {
                 std::cout << to_string(props.memoryTypes[k].propertyFlags) << "\n";
-                if ((vk::MemoryPropertyFlagBits::eHostVisible & props.memoryTypes[k].propertyFlags) &&
-                    (vk::MemoryPropertyFlagBits::eHostCoherent & props.memoryTypes[k].propertyFlags) &&
+                if ((vk::MemoryPropertyFlagBits::eHostVisible &
+                     props.memoryTypes[k].propertyFlags) &&
+                    (vk::MemoryPropertyFlagBits::eHostCoherent &
+                     props.memoryTypes[k].propertyFlags) &&
                     (memorySize < props.memoryHeaps[props.memoryTypes[k].heapIndex].size))
                 {
                     return k;
@@ -163,39 +191,46 @@ int main()
         memory.unmapMemory();
 
         constexpr std::array<vk::DescriptorSetLayoutBinding, 2> bindings = {
-            vk::DescriptorSetLayoutBinding(0, vk::DescriptorType::eStorageBuffer, 1, vk::ShaderStageFlagBits::eCompute,
-                                           nullptr),
-            vk::DescriptorSetLayoutBinding(1, vk::DescriptorType::eStorageBuffer, 1, vk::ShaderStageFlagBits::eCompute,
-                                           nullptr)};
+            vk::DescriptorSetLayoutBinding(0, vk::DescriptorType::eStorageBuffer, 1,
+                                           vk::ShaderStageFlagBits::eCompute, nullptr),
+            vk::DescriptorSetLayoutBinding(1, vk::DescriptorType::eStorageBuffer, 1,
+                                           vk::ShaderStageFlagBits::eCompute, nullptr)};
 
         const auto descriptorSetLayout = vk::raii::DescriptorSetLayout(
-            device, vk::DescriptorSetLayoutCreateInfo(vk::DescriptorSetLayoutCreateFlags(), bindings));
+            device,
+            vk::DescriptorSetLayoutCreateInfo(vk::DescriptorSetLayoutCreateFlags(), bindings));
 
         const auto pipelineLayout = vk::raii::PipelineLayout(
-            device, vk::PipelineLayoutCreateInfo(vk::PipelineLayoutCreateFlags(), *descriptorSetLayout));
+            device,
+            vk::PipelineLayoutCreateInfo(vk::PipelineLayoutCreateFlags(), *descriptorSetLayout));
 
-        const auto shaderModule =
-            vk::raii::ShaderModule(device, vk::ShaderModuleCreateInfo(vk::ShaderModuleCreateFlags(), spirv));
+        const auto shaderModule = vk::raii::ShaderModule(
+            device, vk::ShaderModuleCreateInfo(vk::ShaderModuleCreateFlags(), spirvFromFile));
 
         const auto shaderStageCreateInfo = vk::PipelineShaderStageCreateInfo(
-            vk::PipelineShaderStageCreateFlags(), vk::ShaderStageFlagBits::eCompute, *shaderModule, "f", 0);
+            vk::PipelineShaderStageCreateFlags(), vk::ShaderStageFlagBits::eCompute, *shaderModule,
+            "main");
 
-        const auto computePipelineCreateInfo =
-            vk::ComputePipelineCreateInfo(vk::PipelineCreateFlags(), shaderStageCreateInfo, *pipelineLayout);
+        const auto computePipelineCreateInfo = vk::ComputePipelineCreateInfo(
+            vk::PipelineCreateFlags(), shaderStageCreateInfo, *pipelineLayout);
 
         const auto pipeline = vk::raii::Pipeline(device, nullptr, computePipelineCreateInfo);
 
         constexpr auto DescriptorCount = 2;
-        constexpr auto descriptorPoolSize = vk::DescriptorPoolSize(vk::DescriptorType::eStorageBuffer, DescriptorCount);
+        constexpr auto descriptorPoolSize =
+            vk::DescriptorPoolSize(vk::DescriptorType::eStorageBuffer, DescriptorCount);
         constexpr std::array descriptorPoolSizeArray = {descriptorPoolSize};
         const auto descriptorPoolCreateInfo = vk::DescriptorPoolCreateInfo(
-            vk::DescriptorPoolCreateFlags() | vk::DescriptorPoolCreateFlagBits::eFreeDescriptorSet, 1, descriptorPoolSizeArray);
+            vk::DescriptorPoolCreateFlags() | vk::DescriptorPoolCreateFlagBits::eFreeDescriptorSet,
+            1, descriptorPoolSizeArray);
 
-        assert(descriptorPoolCreateInfo.flags & vk::DescriptorPoolCreateFlagBits::eFreeDescriptorSet);
+        assert(descriptorPoolCreateInfo.flags &
+               vk::DescriptorPoolCreateFlagBits::eFreeDescriptorSet);
 
         const auto descriptorPool = vk::raii::DescriptorPool(device, descriptorPoolCreateInfo);
 
-        const vk::DescriptorSetAllocateInfo descriptorSetAllocateInfo(*descriptorPool, *descriptorSetLayout);
+        const vk::DescriptorSetAllocateInfo descriptorSetAllocateInfo(*descriptorPool,
+                                                                      *descriptorSetLayout);
 
         const auto descriptorSets = device.allocateDescriptorSets(descriptorSetAllocateInfo);
         assert(descriptorSets.size() == 1);
@@ -203,9 +238,9 @@ int main()
 
         // Create in/out buffers with descriptors and bind to memory
         const std::array indices = {*queueFamilyIndex};
-        const auto bufferCreateInfo =
-            vk::BufferCreateInfo(vk::BufferCreateFlags(), bufferSize, vk::BufferUsageFlagBits::eStorageBuffer,
-                                 vk::SharingMode::eExclusive, indices);
+        const auto bufferCreateInfo = vk::BufferCreateInfo(vk::BufferCreateFlags(), bufferSize,
+                                                           vk::BufferUsageFlagBits::eStorageBuffer,
+                                                           vk::SharingMode::eExclusive, indices);
         const auto in_buffer = vk::raii::Buffer(device, bufferCreateInfo);
 
         const auto out_buffer = vk::raii::Buffer(device, bufferCreateInfo);
@@ -215,28 +250,34 @@ int main()
         out_buffer.bindMemory(*memory, bufferSize);
 
         const auto in_descriptorBufferInfo = vk::DescriptorBufferInfo(*in_buffer, 0, VK_WHOLE_SIZE);
-        const auto out_descriptorBufferInfo = vk::DescriptorBufferInfo(*out_buffer, 0, VK_WHOLE_SIZE);
+        const auto out_descriptorBufferInfo =
+            vk::DescriptorBufferInfo(*out_buffer, 0, VK_WHOLE_SIZE);
 
         constexpr auto inBindingIndex = 0;
         constexpr auto outBindingIndex = 1;
         const std::array writeDescriptorSet = {
-            vk::WriteDescriptorSet(*descriptorSet, inBindingIndex, 0, 1, vk::DescriptorType::eStorageBuffer, nullptr,
+            vk::WriteDescriptorSet(*descriptorSet, inBindingIndex, 0, 1,
+                                   vk::DescriptorType::eStorageBuffer, nullptr,
                                    &in_descriptorBufferInfo),
-            vk::WriteDescriptorSet(*descriptorSet, outBindingIndex, 0, 1, vk::DescriptorType::eStorageBuffer, nullptr,
+            vk::WriteDescriptorSet(*descriptorSet, outBindingIndex, 0, 1,
+                                   vk::DescriptorType::eStorageBuffer, nullptr,
                                    &out_descriptorBufferInfo)};
         device.updateDescriptorSets(writeDescriptorSet, {});
 
-        const auto commandPool =
-            vk::raii::CommandPool(device, vk::CommandPoolCreateInfo(vk::CommandPoolCreateFlags(), *queueFamilyIndex));
+        const auto commandPool = vk::raii::CommandPool(
+            device, vk::CommandPoolCreateInfo(vk::CommandPoolCreateFlags(), *queueFamilyIndex));
 
         constexpr auto commandBuffersCount = 1;
         const auto commandBuffers = vk::raii::CommandBuffers(
-            device, vk::CommandBufferAllocateInfo(*commandPool, vk::CommandBufferLevel::ePrimary, commandBuffersCount));
+            device, vk::CommandBufferAllocateInfo(*commandPool, vk::CommandBufferLevel::ePrimary,
+                                                  commandBuffersCount));
 
         const auto& commandBuffer = commandBuffers.front();
-        commandBuffer.begin(vk::CommandBufferBeginInfo(vk::CommandBufferUsageFlagBits::eOneTimeSubmit));
+        commandBuffer.begin(
+            vk::CommandBufferBeginInfo(vk::CommandBufferUsageFlagBits::eOneTimeSubmit));
         commandBuffer.bindPipeline(vk::PipelineBindPoint::eCompute, *pipeline);
-        commandBuffer.bindDescriptorSets(vk::PipelineBindPoint::eCompute, *pipelineLayout, 0, *descriptorSet, nullptr);
+        commandBuffer.bindDescriptorSets(vk::PipelineBindPoint::eCompute, *pipelineLayout, 0,
+                                         *descriptorSet, nullptr);
 
         commandBuffer.dispatch(bufferLength, 1, 1);
         commandBuffer.end();
@@ -247,7 +288,8 @@ int main()
         queue.submit(vk::SubmitInfo(nullptr, nullptr, *commandBuffer));
         queue.waitIdle();
 
-        const auto* payload = static_cast<int32_t*>(memory.mapMemory(0, memorySize, vk::MemoryMapFlags{0}));
+        const auto* payload =
+            static_cast<int32_t*>(memory.mapMemory(0, memorySize, vk::MemoryMapFlags{0}));
         const auto outputSpan = std::span(payload, copyOfInputData.size());
 
         assert(memorySize / sizeof(*payload) == outputSpan.size());
@@ -267,8 +309,8 @@ int main()
         const auto [i1, i2] = std::ranges::mismatch(copyOfInputData, outputSpan);
         if (i1 != copyOfInputData.end())
         {
-            std::cout << "Input and Output differ at " << std::distance(copyOfInputData.begin(), i1) << "/"
-                      << copyOfInputData.size() << "\n";
+            std::cout << "Input and Output differ at " << std::distance(copyOfInputData.begin(), i1)
+                      << "/" << copyOfInputData.size() << "\n";
         }
     }
     return 0;
